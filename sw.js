1a96e7c804cf2de3542e7024b41f28751ce3cc53
b7516555-max <b7516555@gmail.com>
2026-08-26 21:36:34 +0800

    將附近雷達改為美食旋轉輪盤

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'together-eat-shell-v3';
+const CACHE_NAME = 'together-eat-shell-v4';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
