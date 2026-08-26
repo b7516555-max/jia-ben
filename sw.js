ff688f1759b02496b6f96faeeee8f72522709fb9
b7516555-max <b7516555@gmail.com>
2026-08-26 21:32:43 +0800

    新增可選美食人偶並改善電腦版歡迎卡片

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'together-eat-shell-v2';
+const CACHE_NAME = 'together-eat-shell-v3';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
