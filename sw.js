98641c04b6db1801388a3438cc6cf936ff7d70d9
b7516555-max <b7516555@gmail.com>
2026-08-26 21:41:10 +0800

    新增智慧店家國家縣市分類自動配對

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'together-eat-shell-v4';
+const CACHE_NAME = 'together-eat-shell-v5';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
