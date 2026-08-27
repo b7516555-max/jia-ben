2ea44763e407b8a8280d0f6f75aaca756ee8ff1f
b7516555 <b7516555@gmail.com>
2026-08-27 15:21:18 +0800

    fix(avatar): 修正人偶 Sprite 座標定位與縮放避免頭部截斷

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'together-eat-shell-v9';
+const CACHE_NAME = 'together-eat-shell-v10';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
